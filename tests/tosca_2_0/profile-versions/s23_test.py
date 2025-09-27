636d9a19c08e77d3061e44ab5c6d49c0bda25f30
Paul Jordan <paul.m.jordan@outlook.com>
2025-09-27 09:50:29 +0100

    add invalid tests for sections up to 6.5

--- tests/tosca_2_0/profile-versions/s23_test.py
+++ tests/tosca_2_0/profile-versions/s23_test.py
@@ -21,5 +21,5 @@ class TestWrapperProgram(unittest.TestCase):
     def test_wrapper_with_version_yaml(self):
         command = (f'python3 {wrapper_path} {tosca_file_path}')
         result = subprocess.run(command, capture_output=True, text=True, shell=True)
-        self.assertEqual(result.returncode, 0, 
-                         f"Expected return code 0, but got {result.returncode} with message\n {result.stdout}")
+        self.assertEqual(result.returncode, 1, 
+                         f"Expected return code 1, but got {result.returncode} with message\n {result.stdout}")
