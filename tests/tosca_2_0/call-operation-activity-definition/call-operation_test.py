95092e1d62dd19012770ec2b3f7a4e29daffd7a2
Paul Jordan <paul.m.jordan@outlook.com>
2025-10-13 19:27:02 +0100

    add py files for test yaml files

---
+++ tests/tosca_2_0/call-operation-activity-definition/call-operation_test.py
@@ -0,0 +1,17 @@
+# filepath: /home/paul/tosca-community-contributions/tools/scripts/../../tests/tosca_2_0/call-operation-activity-definition/call-operation_test.py
+import subprocess
+import unittest
+import os
+from pathlib import Path
+
+here = os.path.dirname(os.path.abspath(__file__))
+tosca_file_path = here + '/call-operation.yaml'
+wrapper_path = here + '/../../../tools/wrappers/wrapper.py'
+
+class TestWrapperProgram(unittest.TestCase):
+    def test_wrapper_with_yaml(self):
+        command = f'python3 {wrapper_path} {tosca_file_path}'
+        result = subprocess.run(command, capture_output=True, text=True, shell=True)
+        # Expected pass
+        self.assertEqual(result.returncode, 0,
+                         f"Expected return code 0, but got {result.returncode} with message\n {result.stdout}")
