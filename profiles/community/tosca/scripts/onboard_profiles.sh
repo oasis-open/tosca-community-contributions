177167b1aa9a0f29cd9c1b219919c12c4b866430
Chris Lauwers <lauwers@ubicity.com>
2025-11-04 12:53:04 -0800

    Flesh out online boutique example using substitution mapping approach (#241)
    * Add files via upload
    
    * Delete unused file
    
    * Introduce common profile
    
    * Continue fleshing out online boutique example
    
    * Add old version of file
    
    * Support for container ports
    
    * Environment variables are strings
    
    * Rename common profile to core profile

--- profiles/community/tosca/scripts/onboard_profiles.sh
+++ profiles/community/tosca/scripts/onboard_profiles.sh
@@ -8,6 +8,10 @@ HERE=$(dirname "$(readlink --canonicalize "$BASH_SOURCE")")
 # Make sure CSAR directory exists
 mkdir -p ${CSARS_DIR}
 
+echo Creating core:0.1 CSAR file 
+cd ${PROFILES_DIR}/core
+zip -r ${CSARS_DIR}/core.0.1.csar . > /dev/null
+
 echo Creating platform:0.1 CSAR file 
 cd ${PROFILES_DIR}/platform
 zip -r ${CSARS_DIR}/platform.0.1.csar . > /dev/null
@@ -21,6 +25,9 @@ cd ${PROFILES_DIR}/microservices
 zip -r ${CSARS_DIR}/microservices.0.1.csar . > /dev/null
 
 # Add profiles
+echo Onboarding core:0.1
+ubicity profile add ${CSARS_DIR}/core.0.1.csar > /dev/null
+
 echo Onboarding platform:0.1
 ubicity profile add ${CSARS_DIR}/platform.0.1.csar > /dev/null
 
