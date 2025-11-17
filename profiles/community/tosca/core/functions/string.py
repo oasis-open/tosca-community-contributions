93d9e454463b4b424caf15864c4c1d4e224a101a
Roberto-Mameli-Eng <145329027+Roberto-Mameli-Eng@users.noreply.github.com>
2025-11-17 22:07:47 +0100

    Abstract profiles drafted for community discussion (#247)
    * Abstract profiles drafted for community discussion
    
    * Common profile renamed (base). Function implementation added to core profile

---
+++ profiles/community/tosca/core/functions/string.py
@@ -0,0 +1,12 @@
+def to_string(args):
+    if args and args[0] != None:
+        return str(args[0])
+    else:
+        return None
+
+def to_uppercase(args):
+    if args and args[0]:
+        return args[0].upper()
+    else:
+        return None
+
