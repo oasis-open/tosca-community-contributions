b0e9af93777fb41d5ff0634966c8ed412a27e14b
pmjordan <paul.m.jordan@outlook.com>
2025-12-22 17:30:46 +0000

    add tests for spec section 7

---
+++ tests/tosca_2_0/node-template/node-template-copy_test.py
@@ -0,0 +1,54 @@
+import subprocess
+import unittest
+import os
+import zipfile
+from pathlib import Path
+
+
+# Set the path to the tosca file
+here = os.path.dirname(os.path.abspath(__file__))
+# To save re-typing the tosca file name into every test file,
+# guess that the tosca file has the same root as the name of this test file
+# and that the extension is .yaml
+tosca_file_path = here + '/' + Path(__file__).stem.split("_test")[0] + ".yaml"
+# If not the case, then uncomment the following line and give the correct name of the tosca file
+# tosca_file_path = here + '/version.yaml'
+tosca_file_name = os.path.basename(tosca_file_path)
+
+# Create a zip file to create a csar format file
+zip_name = here + '/' + Path(tosca_file_path).with_suffix('.zip').name
+with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zf:
+    # Add the TOSCA.meta content directly using writestr
+    tosca_meta_content = f"CSAR-Version: 2.0\nCreated-By: paul.m.jordan@outlook.com\nEntry-Definitions: {tosca_file_name}\n"
+    zf.writestr('TOSCA.meta', tosca_meta_content)
+    
+    # Add the entry tosca file (use the basename of the tosca file path)
+    zf.write(tosca_file_path, arcname=os.path.basename(tosca_file_path))
+    
+    # Now add the files referenced by the entry tosca file to the archive
+    extra_files = [
+        'container-images/myapp',
+        'scripts/install_mysql.sh'
+    ]
+
+    # Write out the zip entries for each candidate if it exists
+    for file in extra_files:
+        path = os.path.join(here, file)
+        if os.path.exists(path):
+            zf.write(path, arcname=file)
+
+# Set the path to the wrapper
+wrapper_path = here + '/../../../tools/wrappers/wrapper.py'
+
+class TestWrapperProgram(unittest.TestCase):
+
+    def test_wrapper_with_version_yaml(self):
+        command = (f'python3 {wrapper_path} {tosca_file_path}')
+        result = subprocess.run(command, capture_output=True, text=True, shell=True)
+        self.assertEqual(result.returncode, 0, 
+                        f"Expected return code 0, but got {result.returncode} with message\n {result.stdout}\nLeaving zip file for inspection")
+        # If the assertion passes, remove the zip file
+        try:
+            os.remove(zip_name)
+        except FileNotFoundError:
+            pass
