8cc54be6018337c0891a5ecac0812482e6a772a2
Paul Jordan <paul.m.jordan@outlook.com>
2026-03-25 19:56:32 +0000

    Changed ubity wrapper to 2.5, renamed existing wrapper to 2.4 (#290)

---
+++ tools/wrappers/wrapper_ubicity_2_5.py
@@ -0,0 +1,82 @@
+import subprocess
+import sys
+import os
+
+import json
+
+def printJsonResponse(pSuccess, pError, pErrorReason, processor_command, result):
+    response = {
+        "wrapper": {
+            "executedCommand": processor_command,
+            "success": pSuccess,
+            "error": pError,
+            "errorReason": pErrorReason
+            #"outputs": "not yet implemented"
+        },
+        "processorResult": {
+            "stdout": result.stdout,
+            "stderr": result.stderr,
+            "return_code": result.returncode
+        }
+    }
+    # Dump JSON and replace escaped newline sequences (\n) with actual newlines
+    json_text = json.dumps(response, indent=2)
+    json_text = json_text.replace('\\n', '\n')
+    print(json_text)
+
+def main():
+    # Confirm that the correct number of arguments were passed
+    if len(sys.argv) != 2:
+        print("Wrapper Usage: python wrapper_ubicity.py <path_to_file>")
+        sys.exit(101)
+
+    # Confirm that the file exists
+    file_path = sys.argv[1]
+    if not os.path.isfile(file_path):
+        print(f"Wrapper Error: {file_path} is not a valid file.")
+        sys.exit(102)
+
+    # Get file extension
+    file_ext = os.path.splitext(file_path)[1].lower()
+    
+    # Initialize variables
+    tosca_meta_file_path = None
+    file_dir = os.path.dirname(file_path)
+
+    # Initialize variables
+    pSuccess = False
+    pError = False
+    pErrorReason = ""
+
+    # Run the processor
+    processor_command = ["ubicity", "catalog",  "validate", file_path]
+    result = subprocess.run(  
+        processor_command,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    text=True
+    )
+
+    
+    # Condition if the TOSCA file was processed successfully
+    if ((result.returncode == 0)):
+        pSuccess = True
+        printJsonResponse(pSuccess, pError, pErrorReason, processor_command, result)
+        sys.exit(0)
+    # Condition if the file was not valid TOSCA. Error codes above 100
+    # indicate invalid TOSCA.
+    elif ((result.returncode >= 100)):
+        pError = True
+        pErrorReason = "InvalidTOSCA"
+        printJsonResponse(pSuccess, pError, pErrorReason, processor_command, result)
+        sys.exit(1)
+    else:
+        print(f"Wrapper got error from processor: An unknown error occurred with exit code {result.returncode}, error {result.stderr}, and stdout {result.stdout}")
+        pError = True
+        pErrorReason = "Unknown"
+        printJsonResponse(pSuccess, pError, pErrorReason, processor_command, result)
+        sys.exit(99)
+    ################################################
+
+if __name__ == "__main__":
+    main()
